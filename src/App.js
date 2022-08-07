55864d35152b970bf645fec6e0cf3acac5b9c60e
Asadbek <a.nosirjonov@uic.group>
2022-08-08 02:46:34 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [active, setActive] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-white flex gap-10">
+      <div className={active ? 'w-12 ease-in-out duration-300 bg-black h-screen' : 'w-72 bg-black h-screen ease-in-out duration-300'}>
+        <div className='flex items-center justify-between px-2 pt-2'>
+          <div className='cursor-pointer' onClick={() => setActive(!active)}>logo</div>
+          <button onClick={() => setActive(!active)} className={active ? 'hidden' : 'block'}>=</button>
+        </div>
+      </div>
+      <div className='w-full text-black'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus accusantium praesentium incidunt recusandae saepe cum dolorem impedit eligendi aut iure excepturi aperiam similique corporis, sequi suscipit, explicabo sunt rerum soluta!
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="flex flex-col">
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+      <button>f</button>
+    </div>
+  );
+}
+
+export default Sidebar;
