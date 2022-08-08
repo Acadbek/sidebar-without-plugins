ac6e47e9bb2a9cd81f0e7c57274c39f9749a911c
Asadbek <a.nosirjonov@uic.group>
2022-08-08 16:11:26 +0500

    feat: btns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
-import CollapseMenu from './components/collapse';
 import Sidebar from './components/Sidebar';
 
 function App() {
   const [active, setActive] = useState(false)
+
+  const open = () => {
+    console.log('div open');
+  }
+
+  const openWithBtn = () => {
+    console.log('Open with btn');
+  }
+
   return (
     <div className="App text-white flex gap-10">
       <div className={active ? 'w-14 ease-in-out duration-300 bg-black h-screen' : 'w-72 bg-black h-screen ease-in-out duration-300'}>
@@ -14,21 +22,7 @@ function App() {
         </div>
         <div className='flex gap-8 px-4'>
           <div className='mt-7'>
-            <Sidebar />
-          </div>
-          <div className={active ? 'hidden ease-in-out duration-700' : 'flex flex-col gap-6 mt-[33px] ease-in-out duration-700'}>
-            <CollapseMenu />
-            <p>Google</p>
-            <p>Figma</p>
-            <p>Telegram</p>
-            <p>Twitch</p>
-            <p>YouTube</p>
-            <p>Twitter</p>
-            <p>Facebook</p>
-            <p>Pinterest</p>
-            <p>VK</p>
-            <p>Spotify</p>
-            <p>Discord</p>
+            <Sidebar open={open} openWithBtn={openWithBtn} textOpen={active ? true : false} />
           </div>
         </div>
       </div>
