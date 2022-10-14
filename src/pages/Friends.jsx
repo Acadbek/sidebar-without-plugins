9428adcf9511737dc07721c24d23bde957d583e3
Asadbek <a.nosirjonov@uic.group>
2022-10-14 20:15:06 +0500

    change main title

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const Friends = () => {
   return (
-    <div>Friends</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="Friends/old-friends">old frrieds</Route>
+        <Route path="Friends/new-friends">new frrieds</Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default Friends
+export default Friends;
